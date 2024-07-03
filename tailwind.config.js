5041d249539e36dc6945c9b88f8384ceafb7bfd9
naman-398 <you@example.com>
2024-07-03 15:31:14 +0530

    starting

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'light-pastel-purple': '#A18EE8',
+                'light-brown': '#FFB685',
+                'brown': '#E2803E',
+                'rich-black': '#1E2031',
+                'tea-green': "#C5FCC3",
+                'sky': '#AEEFF4',
+                'green': '#4CDFB2',
+                'light-cream': '#FFF8F3',
+                'purple': '#A18EE8',
+                'grey': '#4D4A49',
+                'off-black': '#0E0F13',
+                'light-black': '#2F2F2F',
+                'light-grey': '#E9E9E9',
+                'platinum': '#E5E5E5'
+            },
+            fontSize: {
+                'custom-md': '19px',
+                'custom-5xl': '50px',
+            },
+            fontFamily: {
+                'poppins': ['Poppins', 'sans-serif'],
+                'inter': ['Inter', 'sans-serif'],
+            },
+            lineHeight: {
+                '120': '1.2',
+                '150': '1.5',
+            },
+            boxShadow: {
+                'button-shadow': '2px 2px 0px 0px black',
+                'button-hover-shadow': '-2px -2px 0px 0px black',
+            }
+        }
+    }
+}
